23665c9287fbe7f98f85f60a7f76b6250fa799bb
leoncavalli <50718317+leoncavalli@users.noreply.github.com>
2020-08-23 14:44:48 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Some variables
+            float kilo=70.5f;    
+            string plaka="34PR1177";
+            double km=125425.8;
+            int musteriId=1;
+            bool satistami=true;
+            float maas=2135.80f;
+            string adsoyad="Bünyamin Gültekin";
+            char sube='a';
+            
+            Console.WriteLine(plaka,km,musteriId,satistami,maas,adsoyad,sube);
+
+            //App-Type transformation
+
+            int shortSide;
+            int longSide;
+
+            Console.WriteLine("Please enter the 1. length");
+            int len1=int.Parse(Console.ReadLine());
+            int len2=int.Parse(Console.ReadLine());
+
+            if(len1>len2){
+                shortSide=len2;
+                longSide=len1;
+            }
+            else{
+                shortSide=len1;
+                longSide=len2;
+            }
+
+            var area=longSide*shortSide;
+            Console.WriteLine($"The area is :{area}");
+
+        }
+    }
+}
